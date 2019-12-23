8e6a988061aebc82fb9dbdd6ab2c1bf5e8003337
iDzyubin <iDzyubin@yahoo.com>
2019-12-23 21:09:28 +0300

    Скорректированы имена полей

---
+++ ConferenceApp.Migrator/Migrations/_005.cs
@@ -0,0 +1,28 @@
+﻿using ThinkingHome.Migrator.Framework;
+
+namespace ConferenceApp.Migrator.Migrations
+{
+    [Migration(5)]
+    public class _005 : Migration
+    {
+        #region Script
+
+        private string Script => @"
+            alter table cf.users
+            drop column fax,
+
+            drop column address,
+            add column organisation_address character varying,
+
+            add column city character varying,
+            
+            drop column degree,
+            
+            add column position character varying;
+        ";
+
+        #endregion
+
+        public override void Apply() => Database.ExecuteNonQuery( Script );
+    }
+}
