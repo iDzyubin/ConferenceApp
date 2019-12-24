97177bc214ac49eb15f0cc97310c7bf413478169
iDzyubin <iDzyubin@yahoo.com>
2019-12-24 22:13:50 +0300

    Обновил ViewModel

--- ConferenceApp.Web/ViewModels/UserViewModel.cs
+++ ConferenceApp.Web/ViewModels/UserViewModel.cs
@@ -5,18 +5,18 @@ namespace ConferenceApp.Web.ViewModels
 {
     public class UserViewModel
     {
-        public Guid      Id                 { get; set; } 
-        public string    FirstName          { get; set; } 
-        public string    MiddleName         { get; set; } 
-        public string    LastName           { get; set; } 
-        public Degree    Degree             { get; set; } 
-        public string    Organization       { get; set; } 
-        public string    Address            { get; set; } 
-        public string    Phone              { get; set; } 
-        public string    Fax                { get; set; } 
-        public string    Email              { get; set; } 
-        public DateTime? StartResidenceDate { get; set; } 
-        public DateTime? EndResidenceDate   { get; set; }
-        public UserRole  UserRole           { get; set; }
+        public Guid      Id                  { get; set; } 
+        public string    FirstName           { get; set; } 
+        public string    MiddleName          { get; set; } 
+        public string    LastName            { get; set; } 
+        public string    Position            { get; set; } 
+        public string    Organization        { get; set; } 
+        public string    City                { get; set; }
+        public string    OrganizationAddress { get; set; } 
+        public string    Phone               { get; set; } 
+        public string    Email               { get; set; } 
+        public DateTime? StartResidenceDate  { get; set; } 
+        public DateTime? EndResidenceDate    { get; set; }
+        public UserRole  UserRole            { get; set; }
     }
 }
